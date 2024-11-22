b3e83d7fed9776724b685583e79df58e2b38ab47
Obi <qolbinurwandi@gmail.com>
2024-11-22 22:29:40 +0800

    feat: condition checker node installation

--- Jenkins/01.testing_local_app.sh
+++ Jenkins/01.testing_local_app.sh
@@ -2,15 +2,20 @@ sudo yum update -y
 sudo yum install -y curl
 
 echo "Install Node.js"
-
-curl -fsSL https://fnm.vercel.app/install | bash
-source ~/.bashrc
-
-fnm use --install-if-missing 22
-node -v
-npm -v
+if command -v node &> /dev/null; then
+  echo "Node.js is installed. Skip installation."
+else
+  echo "Node.js is not found. Continue the installation..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+  source ~/.bashrc
+  nvm install node
+  nvm install 16
+  node -v 
+  npm -v
+fi
 
 echo "Testing the local app..."
+cd ..
 
 npm test
 npm start
